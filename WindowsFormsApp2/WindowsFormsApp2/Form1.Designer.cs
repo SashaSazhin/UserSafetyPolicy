cc0524bdcaa0826f7501a648a4bbb5de90bf7ace
Sasha Sazhin <38405849+SashaSazhin@users.noreply.github.com>
2018-05-29 23:54:40 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimeExpiration = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 170);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.HorizontalScrollbar = true;
+            this.listBoxUsers.Location = new System.Drawing.Point(12, 13);
+            this.listBoxUsers.Margin = new System.Windows.Forms.Padding(1);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(250, 264);
+            this.listBoxUsers.TabIndex = 1;
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(339, 21);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(134, 20);
+            this.textBoxLogin.TabIndex = 2;
+            this.textBoxLogin.Text = "Login";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(339, 54);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(134, 20);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.Text = "Qwe123_12";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(339, 103);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(134, 20);
+            this.textBoxEmail.TabIndex = 4;
+            this.textBoxEmail.Text = "qqq@qqq.com";
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(339, 134);
+            this.textBoxPhone.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(134, 20);
+            this.textBoxPhone.TabIndex = 5;
+            this.textBoxPhone.Text = "+380970009988";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(267, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "e-mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(267, 134);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Phone";
+            // 
+            // dateTimeExpiration
+            // 
+            this.dateTimeExpiration.Location = new System.Drawing.Point(339, 77);
+            this.dateTimeExpiration.Margin = new System.Windows.Forms.Padding(1);
+            this.dateTimeExpiration.MinDate = new System.DateTime(2017, 6, 20, 0, 0, 0, 0);
+            this.dateTimeExpiration.Name = "dateTimeExpiration";
+            this.dateTimeExpiration.Size = new System.Drawing.Size(132, 20);
+            this.dateTimeExpiration.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(267, 80);
+            this.label5.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Expiration";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 170);
+            this.button2.Margin = new System.Windows.Forms.Padding(1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 26);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "CheckExpiration";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 287);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimeExpiration);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.listBoxUsers);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBoxUsers;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimeExpiration;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        List<User> Users;
+        List<DateTime> endDate;
+        DialogResult ok_cancel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Users = new List<User>();
+            endDate = new List<DateTime>();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (CheckPsw(textBoxPassword.Text.ToString()))
+            {
+                Users.Add(new User(textBoxLogin.Text, textBoxPassword.Text, textBoxEmail.Text, textBoxPhone.Text, dateTimeExpiration.Value));
+                listBoxUsers.Items.Add(Users[Users.Count - 1].ToString() + '\n');
+            }
+            else
+            {
+                MessageBox.Show("Введите другой пароль!");
+            }
+            endDate.Add(dateTimeExpiration.Value);
+
+        }
+        private bool CheckPsw(string password)
+        {
+            bool checkpass = true;
+            for (int i = 0; i < Users.Count; i++)
+            {
+                if (Users[i].Password.Password == password)
+                {
+                    checkpass = false;
+                    break;
+                }
+            }
+            return checkpass;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Users.Count; i++)
+            {
+                if (Users[i].Password.ExpirationDate < DateTime.Today)
+                {
+                    ok_cancel = MessageBox.Show("Program is going to delete expired password or passwords.\n" + "Press OK to do it and Cancel to stop it",  "DeleteBox", MessageBoxButtons.OKCancel);
+                    break;
+                }
+            }
+
+            if (ok_cancel == DialogResult.OK)
+            {
+                listBoxUsers.Items.Clear();
+                for (int i = 0; i < Users.Count; i++)
+                {
+                    if (endDate[i] <= DateTime.Today)
+                    {
+                        Users.RemoveAt(i);
+                    }
+                    else
+                    {
+                        listBoxUsers.Items.Add(Users[i].ToString() + "\n");
+                    }
+                }
+            }
+
+
+        }
+
+    }
+}
+
